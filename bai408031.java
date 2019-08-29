ae96e80215ada5bb9cdf9e169d6007f53d18bc7d
cong <minhcongvghy@gmail.com>
2019-08-29 17:00:25 +0700

    [Bài tập] Hiển thị lời chào

--- bai408031.java
+++ bai408031.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class bai408031 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello: " + name);
+    }
 }
