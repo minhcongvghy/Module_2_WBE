1de96caea7747a9853bb25118583dcffe956be0f
cong <minhcongvghy@gmail.com>
2019-09-03 17:40:23 +0700

    Làm thêm bài tập:Hiển thị 20 số nguyên tố đầu tiên

--- bai408045.java
+++ bai408045.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class bai408045 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số lượng số nguyên tố");
+        int number = scanner.nextInt();
+        int count = 0;
+        int i = 2;
+        while (count < number) {
+            if (isPrimeNumber(i)) {
+                System.out.print(i + " ");
+                count++;
+            }
+            i++;
+
+        }
+    }
+    public static boolean isPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
