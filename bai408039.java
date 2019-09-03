e987d2f2e75535375222e05666d403340565898a
cong <minhcongvghy@gmail.com>
2019-09-03 16:33:43 +0700

    Làm thêm bài thực hành:Kiểm tra năm nhuận

--- bai408039.java
+++ bai408039.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class bai408039 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a year: ");
+         int year = scanner.nextInt();
+        boolean isLeapYear = false;
+
+        boolean isDivisibleBy4 = year % 4 == 0;
+        if(isDivisibleBy4){
+            boolean isDivisibleBy100 = year % 100 == 0;
+            if(isDivisibleBy100){
+                boolean isDivisibleBy400 = year % 400 == 0;
+                if(isDivisibleBy400){
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+
+        if(isLeapYear){
+            System.out.printf("%d is a leap year", year);
+        } else {
+            System.out.printf("%d is NOT a leap year", year);
+        }
+    }
 }
