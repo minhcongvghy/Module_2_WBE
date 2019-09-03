6163c609bf353a3fc468fbdf397608e7a0945de6
cong <minhcongvghy@gmail.com>
2019-09-03 17:39:53 +0700

    Làm thêm bài tập:Hiển thị các số nguyên tố nhỏ hơn 100

--- bai408044.java
+++ bai408044.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class bai408044 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            if (isPrimeNumber(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    public static boolean isPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
