dacf3b94dae83c6e21aeee2925ca52af7a53cd6f
cong <minhcongvghy@gmail.com>
2019-09-03 17:39:18 +0700

    Làm thêm bài tập:Ứng dụng chuyển đổi tiền tệ

--- bai408047.java
+++ bai408047.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class bai408047 {
+    public static void main(String[] args) {
+        double vnd = 23000;
+        double usd;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số tiền usd ");
+        usd = scanner.nextDouble();
+        double result = usd * vnd;
+        System.out.println(usd + " usd = " + result + " vnd");
+    }
 }
